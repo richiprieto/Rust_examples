268be6e42b07eb1f98628a29ff88b9947858b778
richiprieto <richiprieto@crice.org>
2019-02-18 21:05:56 -0500

    Example program using structs: Rectangles

---
+++ Cap5/rectangles/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 80;
+
+    println!("El area es {} ", area(width1, height1));
+
+}
+
+fn area(width: u32, height1: u32) -> u32{
+    width * height1
+}
