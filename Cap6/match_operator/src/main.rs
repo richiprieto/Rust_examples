03b78add7441ab73fafa471e8e759583de522c73
richiprieto <richiprieto@crice.org>
2019-02-20 11:39:37 -0500

    The match control flow operator

---
+++ Cap6/match_operator/src/main.rs
@@ -0,0 +1,27 @@
+//match  es un operador que permite comparar valores
+//con una serie de patrones y ejecutar el codigo
+//si un patron se empareja con el valor evaluado
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Centavo");
+            1 //retorna este valor
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let valor = value_in_cents(Coin::Penny);
+    println!("El valor es {}", valor);
+}
