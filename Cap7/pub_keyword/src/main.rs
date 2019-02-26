4dc0f3b1fc12019756aafba7fb0fe0a263495880
richiprieto <richiprieto@crice.org>
2019-02-26 11:54:39 -0500

    Using the pub keyword to Make items public

---
+++ Cap7/pub_keyword/src/main.rs
@@ -0,0 +1,13 @@
+mod sound {
+    pub mod instrument{
+        pub fn clarinete() {
+            //Body
+        }
+    }
+}
+
+fn main () {
+    crate::sound::instrument::clarinete();
+
+    sound::instrument::clarinete();
+}
