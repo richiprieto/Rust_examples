22c42b91f1f31dcbf04086fb9d30d180299277db
richiprieto <richiprieto@crice.org>
2019-03-08 13:51:05 -0500

    Nested paths for cleaning up large use list

---
+++ Cap7/nested_paths/src/main.rs
@@ -0,0 +1,13 @@
+// Cuando usa muchos items definidos por el mismo paquete
+// se puede proceder a anidar
+
+// Codigo sin anidar
+// use std::cmp::Ordering;
+// use std::io;
+
+//Codigo anidado
+use std::{cmp::Ordering, io};
+
+fn main() {
+    unimplemented!();
+}
