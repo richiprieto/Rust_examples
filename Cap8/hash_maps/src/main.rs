33a2c7cca17669676ba538512e9a00ff39b4228c
richiprieto <richiprieto@crice.org>
2019-03-29 19:10:07 -0500

    Hashmaps and Ownership incompleto...

--- Cap8/hash_maps/src/main.rs
+++ Cap8/hash_maps/src/main.rs
@@ -26,4 +26,13 @@ fn main() {
     println!("{:?}", scores);
 
     // Hashmaps y Ownership
+    let nombre = String::from("Color favorito");
+    let valor = String::from("Negro");
+
+    let mut map = HashMap::new();
+    map.insert(nombre, valor);
+    // en este punto las variables nombre y valor han sido movido al hashmap mediante insert
+    // si insertamos valores por referencia al hashmap los valores no seran movidos en propiedad
+    // dentro del hashmap
+    
 }
