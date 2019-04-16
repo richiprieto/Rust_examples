4df5fcd4550ba95380805064f93772aa900550f4
richiprieto <richiprieto@crice.org>
2019-04-16 00:56:51 -0500

    Introduccion, valida texto y Salida Aun incompleto el desarrollo del software

--- Others/text_interface/src/main.rs
+++ Others/text_interface/src/main.rs
@@ -5,6 +5,39 @@ Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of 
 in a department or all people in the company by department, sorted
 alphabetically.
 */
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        intro(); // Introduccion del software
+        let input_value = input_text(); // Valida que el valor sea un numero
+        match input_value { // Validamos las opciones
+            3 => break, // Salir con valor 3
+            _ => println!("valor {}", input_value),
+        };
+    }
+}
+
+fn intro(){
+    println!("Por favor ingrese la opcion:");
+    println!("1) Agregar un empleado");
+    println!("2) Consultar empleados");
+    println!("3) Salir");
+}
+
+fn input_text() -> u8 {
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Falla a leer stdin");
+
+    let trimmed = input_text.trim();
+
+    match trimmed.parse::<u8>(){
+        Ok(i) => return i,
+        Err(..) => {
+            println!("No es un numero");
+            return 3;
+        },
+    };
 }
