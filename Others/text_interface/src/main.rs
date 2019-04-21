d9bef96fc1feaa20f0b86defbce0710adcca31c6
richiprieto <richiprieto@crice.org>
2019-04-21 00:49:31 -0500

    Agregando colorized y add_user func Incompleto todavia existe un error cuando se llama a hashmap desde la funcion add_user

--- Others/text_interface/src/main.rs
+++ Others/text_interface/src/main.rs
@@ -5,29 +5,20 @@ Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of 
 in a department or all people in the company by department, sorted
 alphabetically.
 */
-use std::io;
-use std::collections::HashMap;
 
-fn main() {
-    loop {
-        intro(); // Introduccion del software
-        let input_value = input_text(); // Valida que el valor sea un numero
-        match input_value { // Validamos las opciones
-            1 => add_user(),
-            3 => break, // Salir con valor 3
-            _ => println!("No es una opcion valida."),
-        };
-    }
-}
+use std::collections::HashMap;
+extern crate colored;
+use colored::*;
 
 fn intro(){
-    println!("Por favor ingrese la opcion:");
+    println!("{}", "Por favor ingrese la opcion:".green());
     println!("1) Agregar un empleado");
     println!("2) Consultar empleados");
     println!("3) Salir");
 }
 
-fn input_text() -> u8 {
+fn input_number() -> u8 {
+    use std::io;
     // variable tipo string
     let mut input_text = String::new();
     // ingreso el valor
@@ -40,12 +31,48 @@ fn input_text() -> u8 {
     match trimmed.parse::<u8>(){
         Ok(i) => return i,
         Err(..) => {
-            println!("No es un numero");
+            println!("{}", "No es un numero, Saliendo...".red());
             return 3;
         },
     };
 }
 
+fn input_text() -> String {
+    use std::io;
+    // variable tipo string
+    let mut input_text = String::new();
+    // ingreso el valor
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Falla a leer stdin");
+
+    let trimmed = input_text.trim();
+
+    trimmed.to_string()
+}
+
 fn add_user() {
-    println!("Ejemplo de Ingreso: Add Amir to Sales." );
+
+    let mut book_reviews = HashMap<&str, &str>::new();
+    loop {
+        println!("{}", "Ejemplo de Ingreso: Add Amir to Sales or Add Sally to Engineering".green());
+        let input_text = input_text(); // Ingreso de texto
+        let v: Vec<&str> = input_text.split(' ').collect(); // Lo spliteamos y agregamos a un vector
+        if v[0].to_lowercase() == "add" && v[2].to_lowercase() == "to"{
+            println!("simon");
+        }
+        break;
+    }
+}
+
+fn main() {
+    loop {
+        intro(); // Introduccion del software
+        let input_number = input_number(); // Valida que el valor sea un numero
+        match input_number { // Validamos las opciones
+            1 => add_user(),
+            3 => break, // Salir con valor 3
+            _ => println!("No es una opcion valida."),
+        };
+    }
 }
